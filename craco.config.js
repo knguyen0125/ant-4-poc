2bb38f7dada200d6923e1beec6e12394e39908be
Kien Nguyen <k.nguyen0125@gmail.com>
2020-03-31 12:41:16 +0700

    Add antd style variables

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,26 @@
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const WebpackBar = require("webpackbar");
-const CracoAntDesignPlugin = require('craco-antd')
+const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackBar = require('webpackbar');
+const CracoAntDesignPlugin = require('craco-antd');
 
 module.exports = {
   babel: {
-    plugins: [
-      "lodash",
-      "@loadable/babel-plugin",
-    ]
+    plugins: ['lodash', '@loadable/babel-plugin'],
   },
   webpack: {
     plugins: [
       new WebpackBar({ profile: true }),
-      ...(process.env.ANALYZE === "true"
+      ...(process.env.ANALYZE === 'true'
         ? [new BundleAnalyzerPlugin({ openAnalyzer: true })]
-        : [])
-    ]
+        : []),
+    ],
   },
   plugins: [
-    {plugin: CracoAntDesignPlugin}
-  ]
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeThemeLessPath: path.join(__dirname, 'src/styles/theme.less'),
+      },
+    },
+  ],
 };
